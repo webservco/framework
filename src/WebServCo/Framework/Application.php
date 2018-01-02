c0ce456056bd2d022fb23974ec017060009679da
Radu Maier <ariademur@webserv.co>
2018-01-02 21:43:29 +0100

    Basic Application class

--- src/WebServCo/Framework/Application.php
+++ src/WebServCo/Framework/Application.php
@@ -5,5 +5,31 @@ use WebServCo\Framework\Framework as Fw;
 
 class Application
 {
+    public function __construct($dirPublic, $dirProject)
+    {
+        $pathPublic = "{$dirPublic}/";
+        $pathProject = realpath($dirProject . '/..') . '/';
+        
+        Fw::config()->set('app.path.public', $pathPublic);
+        Fw::config()->set('app.path.project', $pathProject);
+    }
     
+    public function boot()
+    {
+        //config
+        
+        if (Fw::isCLI()) {
+            return $this->bootCLI();
+        } else {
+            return $this->bootHTTP();
+        }
+    }
+    
+    protected function bootCLI()
+    {
+    }
+    
+    protected function bootHTTP()
+    {
+    }
 }
