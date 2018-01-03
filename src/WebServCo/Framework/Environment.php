b9819009f0deda9fd3bbbe3068c1de916e6d1770
Radu Maier <ariademur@webserv.co>
2018-01-03 16:27:30 +0100

    Add docblocks to Environment and Framework classes

--- src/WebServCo/Framework/Environment.php
+++ src/WebServCo/Framework/Environment.php
@@ -7,6 +7,11 @@ final class Environment
     const ENV_TEST = 'test';
     const ENV_PROD = 'prod';
     
+    /**
+     * Retrieve list of all possible environment options.
+     *
+     * @return array
+     */
     final public static function getOptions()
     {
         return [self::ENV_DEV, self::ENV_TEST, self::ENV_PROD];
--- src/WebServCo/Framework/Framework.php
+++ src/WebServCo/Framework/Framework.php
@@ -3,10 +3,17 @@ namespace WebServCo\Framework;
 
 final class Framework
 {
+    /**
+     * Stores all object instances.
+     */
     private static $libraries;
     
     /**
      * Returns an instance of a Framework Library.
+     *
+     * @param string $className
+     *
+     * @return mixed
      */
     final private static function get($className)
     {
@@ -37,22 +44,33 @@ final class Framework
     
     /**
      * Returns the path the framework project is located in.
+     *
+     * @return string
      */
     final public static function getPath()
     {
         return str_replace('src/WebServCo/Framework', '', __DIR__);
     }
     
+    /**
+     * Checks if interface type is CLI
+     */
     final public static function isCLI()
     {
         return 'cli' === PHP_SAPI;
     }
     
+    /**
+     * Method to access a single instance of the Config Library.
+     */
     final public static function config()
     {
         return self::get('Config');
     }
     
+    /**
+     * Method to access a single instance of the Log Library.
+     */
     final public static function log()
     {
         return self::get('Log');
