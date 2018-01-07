a20c1d80637fbe8c1cec840f68fdcc3b2b1a81f4
Radu Maier <ariademur@webserv.co>
2018-01-07 20:05:09 +0100

    Add Settings class

---
+++ src/WebServCo/Framework/Settings.php
@@ -0,0 +1,11 @@
+<?php
+namespace WebServCo\Framework;
+
+final class Settings
+{
+    /**
+     * Divider used for special strings throughout the Framework,
+     * for example in storing / retrieving ArrayStorage data.
+     */
+    const DIVIDER = '|';
+}
