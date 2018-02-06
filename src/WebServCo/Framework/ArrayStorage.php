65cf4ee066ebf37f3ce490930846582f3b62f80c
Radu Maier <ariademur@webserv.co>
2018-02-06 12:04:00 +0100

    Add unset and clear methods with unit tests

--- src/WebServCo/Framework/ArrayStorage.php
+++ src/WebServCo/Framework/ArrayStorage.php
@@ -22,11 +22,11 @@ final class ArrayStorage
      * @param array $storage
      * @param mixed $setting Can be an array, a string,
      *                          or a special formatted string
-     *                          (eg 'app|path|project').
+     *                          (eg 'app/path/project').
      * @param mixed $defaultValue
      * @return mixed
      */
-    public static function get($storage = [], $setting = null, $defaultValue = false)
+    public static function get($storage, $setting = null, $defaultValue = false)
     {
         $setting = self::parseSetting($setting);
         
@@ -56,7 +56,7 @@ final class ArrayStorage
                     return $storage[$key];
                 }
                 /**
-                 * Go down one lement in the configuration data
+                 * Go down one element in the configuration data
                  * and call the method again, with the remainig setting.
                  */
                 return self::get($storage[$key], $setting, $defaultValue);
@@ -87,15 +87,16 @@ final class ArrayStorage
      * @param array $storage
      * @param mixed $setting Can be an array, a string,
      *                          or a special formatted string
-     *                          (eg 'app|path|project').
+     *                          (eg 'app/path/project').
      * @param mixed $value The value to be stored.
      *
      * @return array The storage array with new data.
+     * @throws \ErrorException
      */
     public static function set($storage, $setting, $value)
     {
         if (!is_array($storage) || empty($setting)) {
-            return $storage;
+            throw new \ErrorException('Invalid parameters specified');
         }
         $setting = self::parseSetting($setting);
         if (is_array($setting)) {
@@ -112,26 +113,124 @@ final class ArrayStorage
     }
     
     /**
+     * Clears a value from a storage array
+     * @param array $storage
+     * @param mixed $setting Can be an array, a string,
+     *                          or a special formatted string
+     *                          (eg 'app/path/project').
+     *
+     * @return array The updates storage array.
+     */
+    public static function clear($storage, $setting)
+    {
+        return self::set($storage, $setting, null);
+    }
+    
+    /**
      * Append data to a storage array.
      *
      * @param array $storage
      * @param mixed $data
      * @return array
+     * @throws \ErrorException
      */
     public static function append($storage, $data = [])
     {
-        if (is_array($storage) && is_array($data)) {
-            foreach ($data as $setting => $value) {
-                if (array_key_exists($setting, $storage) &&
-                    is_array($storage[$setting]) &&
-                    is_array($value)
-                ) {
-                    $storage[$setting] = self::append($storage[$setting], $value);
-                } else {
-                    $storage[$setting] = $value;
-                }
+        if (!is_array($storage) || !is_array($data)) {
+            throw new \ErrorException('Invalid parameters specified');
+        }
+        foreach ($data as $setting => $value) {
+            if (array_key_exists($setting, $storage) &&
+                is_array($storage[$setting]) &&
+                is_array($value)
+            ) {
+                $storage[$setting] = self::append($storage[$setting], $value);
+            } else {
+                $storage[$setting] = $value;
             }
         }
         return $storage;
+    }
+    
+    /**
+     * Removes a setting from a storage array.
+     *
+     * @param array $storage
+     * @param mixed $setting Can be an array, a string,
+     *                          or a special formatted string
+     *                          (eg 'app/path/project').
+     *
+     * @return array The updated storage array.
+     * @throws \ErrorException
+     */
+    public static function unset($storage, $setting)
+    {
+        if (!is_array($storage) || empty($setting)) {
+            throw new \ErrorException('Invalid parameters specified');
+        }
+        
+        $setting = self::parseSetting($setting);
+        
+        if (empty($setting)) {
+            throw new \ErrorException('Empty setting');
+        }
+        
+        if (is_array($setting)) {
+            return self::removeByIndex($storage, $setting);
+        }
+        if (!array_key_exists($setting, $storage)) {
+            throw new \ErrorException(
+                sprintf('"%s" does not exist in storage object', $setting)
+            );
+        }
+        unset($storage[$setting]);
+        return $storage;
+    }
+    
+    /**
+     * Remove index from multi-dimensional array.
+     *
+     * https://stackoverflow.com/questions/26661828/
+     *
+     * @param array $array
+     *   The array to remove the index from.
+     * @param array $indices
+     *   Indexed array containing the indices chain up to the index that should be
+     *   removed.
+     * @return array
+     *   The array with the index removed.
+     * @throws \ErrorException
+     *   If the index does not exist within the array.
+     */
+    protected static function removeByIndex($array, $indices)
+    {
+        if (!is_array($array) || !is_array($indices)) {
+            throw new \ErrorException('Invalid parameters specified');
+        }
+        // Create a reference to the original array.
+        $a = &$array;
+        // Count all passed indices, remove one because arrays are zero based.
+        $c = count($indices) - 1;
+        // Iterate over all passed indices.
+        for ($i = 0; $i <= $c; ++$i) {
+            // Make sure the index to go down for deletion actually exists.
+            if (!array_key_exists($indices[$i], $a)) {
+                throw new \ErrorException(
+                    sprintf('"%s" does not exist in storage object', $indices[$i])
+                );
+            }
+            // This is the target if we reached the last index that was passed.
+            if ($i === $c) {
+                unset($a[$indices[$i]]);
+            } elseif (is_array($a[$indices[$i]])) {
+                // Make sure we have an array to go further down.
+                $a = &$a[$indices[$i]];
+            } else {
+                throw new \ErrorException(
+                    sprintf('"%s" does not exist in storage object', $indices[$i])
+                );
+            }
+        }
+        return $array;
     }
 }
---
+++ tests/unit/WebServCo/Framework/ArrayStorageTest.php
@@ -0,0 +1,165 @@
+<?php
+namespace Tests\Framework;
+
+use PHPUnit\Framework\TestCase;
+
+use WebServCo\Framework\Settings as S;
+use WebServCo\Framework\ArrayStorage;
+
+final class ArrayStorageTest extends TestCase
+{
+    private $originalArray;
+    
+    public function setUp()
+    {
+        $this->originalArray = [
+            'key' => 'value',
+            'foo' => [
+                'bar' => [
+                    'baz' => [
+                        'foobarbaz'
+                    ],
+                ]
+            ],
+        ];
+    }
+    
+    /**
+     * @test
+     * @expectedException \ErrorException
+     */
+    public function unsetWithNonExistingTripleSettingThrowsException()
+    {
+        $setting = sprintf('foo%1$snotBar%1$sbaz', S::DIVIDER);
+        ArrayStorage::unset(
+            $this->originalArray,
+            $setting
+        );
+    }
+    
+    /**
+     * @test
+     * @expectedException \ErrorException
+     */
+    public function unsetWithNonExistingDoubleSettingThrowsException()
+    {
+        $setting = sprintf('foo%1$snotBar', S::DIVIDER);
+        ArrayStorage::unset(
+            $this->originalArray,
+            $setting
+        );
+    }
+    
+    /**
+     * @test
+     * @expectedException \ErrorException
+     */
+    public function unsetWithNonExistingSimpleSettingThrowsException()
+    {
+        ArrayStorage::unset(
+            $this->originalArray,
+            'noexist'
+        );
+    }
+    
+    /**
+     * @test
+     */
+    public function unsetWorksWithTripleSetting()
+    {
+        $setting = sprintf('foo%1$sbar%1$sbaz', S::DIVIDER);
+        $expected = [
+            'key' => 'value',
+            'foo' => [
+                'bar' => [],
+            ],
+        ];
+        $this->assertEquals($expected, ArrayStorage::unset(
+            $this->originalArray,
+            $setting
+        ));
+    }
+    
+    /**
+     * @test
+     */
+    public function unsetWorksWithDoubleSetting()
+    {
+        $setting = sprintf('foo%1$sbar', S::DIVIDER);
+        $expected = [
+            'key' => 'value',
+            'foo' => [],
+        ];
+        $this->assertEquals($expected, ArrayStorage::unset(
+            $this->originalArray,
+            $setting
+        ));
+    }
+    
+    /**
+     * @test
+     */
+    public function unsetWorksWithSimpleSetting()
+    {
+        $expected = [
+            'key' => 'value',
+        ];
+        $this->assertEquals($expected, ArrayStorage::unset(
+            $this->originalArray,
+            'foo'
+        ));
+    }
+    
+    /**
+     * @test
+     */
+    public function clearWorksWithTripleSetting()
+    {
+        $setting = sprintf('foo%1$sbar%1$sbaz', S::DIVIDER);
+        $expected = [
+            'key' => 'value',
+            'foo' => [
+                'bar' => [
+                    'baz' => null,
+                ],
+            ],
+        ];
+        $this->assertEquals($expected, ArrayStorage::clear(
+            $this->originalArray,
+            $setting
+        ));
+    }
+    
+    /**
+     * @test
+     */
+    public function clearWorksWithDoubleSetting()
+    {
+        $setting = sprintf('foo%1$sbar', S::DIVIDER);
+        $expected = [
+            'key' => 'value',
+            'foo' => [
+                'bar' => null,
+            ],
+        ];
+        $this->assertEquals($expected, ArrayStorage::clear(
+            $this->originalArray,
+            $setting
+        ));
+    }
+    
+    /**
+     * @test
+     */
+    public function clearWorksWithSimpleSetting()
+    {
+        $expected = [
+            'key' => 'value',
+            'foo' => null,
+        ];
+        $this->assertEquals($expected, ArrayStorage::clear(
+            $this->originalArray,
+            'foo'
+        ));
+    }
+}
