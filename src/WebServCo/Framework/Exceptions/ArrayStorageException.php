9ebc826d1fb696bec5bf5f53c0971d0f5bafe7ed
Radu Maier <ariademur@webserv.co>
2018-02-19 10:23:09 +0100

    Fix "Opening brace should be on a new line"

--- src/WebServCo/Framework/Exceptions/ArrayStorageException.php
+++ src/WebServCo/Framework/Exceptions/ArrayStorageException.php
@@ -3,11 +3,13 @@ namespace WebServCo\Framework\Exceptions;
 
 final class ArrayStorageException extends \Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null) {
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
         parent::__construct($message, $code, $previous);
     }
     
-    public function __toString() {
+    public function __toString()
+    {
         return sprintf("%s: [%s]: %s\n", __CLASS__, $this->code, $this->message);
     }
 }
