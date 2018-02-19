0f193e62db1b137229625c71b6552791646c0e6f
Radu Maier <ariademur@webserv.co>
2018-02-19 21:42:58 +0100

    Add getAppUrl, getUrl, getQueryString

--- src/WebServCo/Framework/Traits/RequestUrlTrait.php
+++ src/WebServCo/Framework/Traits/RequestUrlTrait.php
@@ -3,7 +3,7 @@ namespace WebServCo\Framework\Traits;
 
 trait RequestUrlTrait
 {
-    public function guessAppUrl()
+    public function getAppUrl()
     {
         if (\WebServCo\Framework\Framework::isCLI()) {
             return false;
@@ -13,5 +13,26 @@ trait RequestUrlTrait
         $this->getHost() .
         $this->path .
         DIRECTORY_SEPARATOR;
+    }
+    
+    public function getUrl($includeQuery = true)
+    {
+        $url = $this->getAppUrl();
+        $url .= $this->getTarget();
+        $url .= $this->getSuffix();
+        if ($includeQuery) {
+            $url .= '?' . $this->getQueryString();
+        }
+        return $url;
+    }
+    
+    public function getQueryString()
+    {
+        $queries = [];
+        $query = $this->getQuery();
+        foreach ($query as $k => $v) {
+            $queries[] = sprintf('%s=%s', $k, $v);
+        }
+        return implode('&', $queries);
     }
 }
