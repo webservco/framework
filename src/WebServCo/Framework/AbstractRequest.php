8acf1a9fb801add4e38bb482a0fa4ce9e1c4b206
Radu Maier <ariademur@webserv.co>
2018-02-19 22:04:11 +0100

    Introduce AbstractRequest (enforce traits methods final keyword)

---
+++ src/WebServCo/Framework/AbstractRequest.php
@@ -0,0 +1,55 @@
+<?php
+namespace WebServCo\Framework;
+
+abstract class AbstractRequest extends \WebServCo\Framework\AbstractLibrary
+{
+    /**
+     * Sanitized _SERVER data.
+     */
+    protected $server = [];
+    /**
+     * Request method.
+     */
+    protected $method;
+    /**
+     * Current script filename. Should most commonly be index.php
+     */
+    protected $filename;
+    /**
+     * Script path.
+     * For HTTP requests this will be public web server subdirectory
+     * the project is located in.
+     * For CLI request this will be the script path
+     * (full or relative, depending on how the script was called).
+     */
+    protected $path = '';
+    /**
+     * Sanitized Framework customized target path.
+     */
+    protected $target = '';
+    /**
+     * Sanitized request query.
+     */
+    protected $query = [];
+    /**
+     * Sanitized Framework customized CLI arguments.
+     *
+     * Excludes the script name and the second argument
+     * which is the Framework customized target path.
+     */
+    protected $args = [];
+    /**
+     * Dummy file extension used in the URL
+     */
+    protected $suffix;
+    
+    use \WebServCo\Framework\Traits\RequestProcessTrait;
+    use \WebServCo\Framework\Traits\RequestServerTrait;
+    use \WebServCo\Framework\Traits\RequestUrlTrait;
+    
+    abstract public function getTarget();
+    
+    abstract public function getQuery();
+    
+    abstract public function getSuffix();
+}
--- src/WebServCo/Framework/Libraries/Request.php
+++ src/WebServCo/Framework/Libraries/Request.php
@@ -1,52 +1,8 @@
 <?php
 namespace WebServCo\Framework\Libraries;
 
-final class Request extends \WebServCo\Framework\AbstractLibrary
+final class Request extends \WebServCo\Framework\AbstractRequest
 {
-    /**
-     * Sanitized _SERVER data.
-     */
-    protected $server = [];
-    /**
-     * Request method.
-     */
-    protected $method;
-    /**
-     * Current script filename. Should most commonly be index.php
-     */
-    protected $filename;
-    /**
-     * Script path.
-     * For HTTP requests this will be public web server subdirectory
-     * the project is located in.
-     * For CLI request this will be the script path
-     * (full or relative, depending on how the script was called).
-     */
-    protected $path = '';
-    /**
-     * Sanitized Framework customized target path.
-     */
-    protected $target = '';
-    /**
-     * Sanitized request query.
-     */
-    protected $query = [];
-    /**
-     * Sanitized Framework customized CLI arguments.
-     *
-     * Excludes the script name and the second argument
-     * which is the Framework customized target path.
-     */
-    protected $args = [];
-    /**
-     * Dummy file extension used in the URL
-     */
-    protected $suffix;
-    
-    use \WebServCo\Framework\Traits\RequestProcessTrait;
-    use \WebServCo\Framework\Traits\RequestServerTrait;
-    use \WebServCo\Framework\Traits\RequestUrlTrait;
-     
     public function __construct($config, $server, $post = [])
     {
         parent::__construct($config);
@@ -54,14 +10,14 @@ final class Request extends \WebServCo\Framework\AbstractLibrary
         $this->init($server, $post);
     }
     
-    public function getQuery()
-    {
-        return $this->query;
-    }
-    
     public function getTarget()
     {
         return $this->target;
+    }
+    
+    public function getQuery()
+    {
+        return $this->query;
     }
     
     public function getArgs()
