333610c107ec24e0509135540808f062e98d63b4
Radu Maier <ariademur@webserv.co>
2018-02-20 11:29:39 +0100

    Introduce AbstractForm

---
+++ src/WebServCo/Framework/AbstractForm.php
@@ -0,0 +1,32 @@
+<?php
+namespace WebServCo\Framework;
+
+abstract class AbstractForm extends \WebServCo\Framework\AbstractLibrary
+{
+    protected $errors = [];
+    
+    use \WebServCo\Framework\Traits\ExposeLibrariesTrait;
+    
+    public function __construct($settings)
+    {
+        parent::__construct($settings);
+        
+        foreach ($this->setting('meta', []) as $field => $title) {
+            /**
+             * Set form data from POST request.
+             */
+            $this->setData($field, $this->request()->data($field));
+        }
+    }
+    
+    abstract protected function db();
+    
+    abstract protected function request();
+    
+    abstract protected function validate();
+    
+    final public function isSent()
+    {
+        return $this->request->getMethod() === \WebServCo\Framework\Http::METHOD_POST;
+    }
+}
