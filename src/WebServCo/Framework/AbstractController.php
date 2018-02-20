2ff542c3891fa2f4da4f4839c63fb34ab63c2bd5
Radu Maier <ariademur@webserv.co>
2018-02-20 10:11:45 +0100

    Add Request type hint

--- src/WebServCo/Framework/AbstractController.php
+++ src/WebServCo/Framework/AbstractController.php
@@ -7,7 +7,7 @@ abstract class AbstractController
     use \WebServCo\Framework\Traits\ExposeLibrariesTrait;
     use \WebServCo\Framework\Traits\ResponseUrlTrait;
     
-    public function __construct($outputLoader)
+    public function __construct(\WebServCo\Framework\AbstractOutputLoader $outputLoader)
     {
         $this->outputLoader = $outputLoader;
     }
