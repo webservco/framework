daa2d58fa1aaa2e68040168ac69a977bbc5ea7db
Radu Maier <ariademur@webserv.co>
2018-03-04 12:41:44 +0100

    Introduce AbstractUser

---
+++ src/WebServCo/Framework/AbstractUser.php
@@ -0,0 +1,8 @@
+<?php
+namespace WebServCo\Framework;
+
+abstract class AbstractUser extends \WebServCo\Framework\AbstractLibrary
+{
+    const ERR_LOGIN = 'ERR_LOGIN';
+    const ERR_DISABLED = 'ERR_DISABLED';
+}
