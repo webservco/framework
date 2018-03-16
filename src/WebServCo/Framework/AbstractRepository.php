b4aef6188171d417f3dde74d93c81cf79e1c45e9
Radu Maier <ariademur@webserv.co>
2018-03-16 12:16:22 +0100

    Refactor AbstractRepository, extends AbstractLibrary

--- src/WebServCo/Framework/AbstractRepository.php
+++ src/WebServCo/Framework/AbstractRepository.php
@@ -1,16 +1,15 @@
 <?php
 namespace WebServCo\Framework;
 
-use WebServCo\Framework\Framework as Fw;
-
-abstract class AbstractRepository
+abstract class AbstractRepository extends \WebServCo\Framework\AbstractLibrary
 {
     use \WebServCo\Framework\Traits\OutputTrait;
     use \WebServCo\Framework\Traits\ExposeLibrariesTrait;
     use \WebServCo\Framework\Traits\ResponseUrlTrait;
-    
+
     public function __construct(\WebServCo\Framework\AbstractOutputLoader $outputLoader)
     {
+        parent::__construct();
         $this->setOutputLoader($outputLoader);
     }
 }
