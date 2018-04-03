f55ee192a3d8e2a4a478c036f88655ecb1733992
Radu Maier <ariademur@webserv.co>
2018-04-03 12:11:40 +0200

    Utils: add isDate, refactor isA to arrayKey

--- src/WebServCo/Framework/AbstractForm.php
+++ src/WebServCo/Framework/AbstractForm.php
@@ -4,17 +4,17 @@ namespace WebServCo\Framework;
 abstract class AbstractForm extends \WebServCo\Framework\AbstractLibrary
 {
     protected $errors;
-    
+
     protected $filtered;
-    
+
     protected $valid;
-    
+
     use \WebServCo\Framework\Traits\ExposeLibrariesTrait;
-    
+
     public function __construct($settings, $defaultData = [])
     {
         parent::__construct($settings);
-        
+
         /**
          * Set form data
          */
@@ -23,42 +23,42 @@ abstract class AbstractForm extends \WebServCo\Framework\AbstractLibrary
                 $field,
                 $this->request()->data( // from POST
                     $field,
-                    \WebServCo\Framework\Utils::isA($defaultData, $field) // default data
+                    \WebServCo\Framework\Utils::arrayKey($field, $defaultData, null) // default data
                 )
             );
         }
-        
+
         $this->errors = [];
-        
+
         $this->filtered = $this->filter();
-        
+
         if ($this->isSent()) {
             $this->valid = $this->validate();
         }
     }
-    
+
     abstract protected function db();
-    
+
     /**
      * @return bool
      */
     abstract protected function filter();
-    
+
     /**
      * @return bool
      */
     abstract protected function validate();
-    
+
     final public function isSent()
     {
         return $this->request()->getMethod() === \WebServCo\Framework\Http::METHOD_POST;
     }
-    
+
     final public function isValid()
     {
         return $this->valid;
     }
-    
+
     final public function toArray()
     {
         return [
--- src/WebServCo/Framework/Utils.php
+++ src/WebServCo/Framework/Utils.php
@@ -3,8 +3,14 @@ namespace WebServCo\Framework;
 
 final class Utils
 {
-    public static function isA($array, $key, $defaultValue = null)
+    public static function arrayKey($key, $array, $defaultValue = false)
     {
         return array_key_exists($key, $array) ? $array[$key] : $defaultValue;
+    }
+
+    public static function isDate(string $date, $format = 'Y-m-d')
+    {
+        $dateTime = \DateTime::createFromFormat($format, $date);
+        return $dateTime && $dateTime->format($format) == $date;
     }
 }
