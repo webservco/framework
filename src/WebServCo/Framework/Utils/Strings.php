8e082f855b48cfd5ad55bf817c091706b4e89a54
Radu Maier <ariademur@webserv.co>
2018-07-05 12:50:54 +0200

    Utils/Strings.endsWith

--- src/WebServCo/Framework/Utils/Strings.php
+++ src/WebServCo/Framework/Utils/Strings.php
@@ -14,6 +14,14 @@ final class Strings
         return 0 === $function($haystack, $needle);
     }
 
+    public static function endsWith($haystack, $needle)
+    {
+        $functionSubstr = function_exists('mb_substr') ? 'mb_substr' : 'substr';
+        $functionStrlen = function_exists('mb_strlen') ? 'mb_strlen' : 'strlen';
+        $check = $functionSubstr($haystack, $functionStrlen($haystack) - ($functionStrlen($needle)));
+        return $check == $needle;
+    }
+
     public static function contains($haystack, $needle, $ignoreCase = true)
     {
         if (false !== $ignoreCase) {
