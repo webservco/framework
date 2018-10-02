fd885fab040c38a82e9d2e641618b912b253d671
Radu Maier <ariademur@webserv.co>
2018-10-02 11:35:51 +0200

    Fix AbstractLogger

--- src/WebServCo/Framework/AbstractLogger.php
+++ src/WebServCo/Framework/AbstractLogger.php
@@ -1,30 +1,8 @@
 <?php
 namespace WebServCo\Framework;
 
-use WebServCo\Framework\Exceptions\ApplicationException;
-use WebServCo\Framework\Interfaces\RequestInterface;
-
 abstract class AbstractLogger
 {
-    protected $channel;
-    protected $logPath;
-    protected $requestInterface;
-
-    public function __construct($channel, $logDir, RequestInterface $requestInterface)
-    {
-        $this->channel = $channel;
-
-        if (!is_readable($logDir)) {
-            throw new ApplicationException('Log dir not readable');
-        }
-        if (!is_writable($logDir)) {
-            throw new ApplicationException('Log dir not writeable');
-        }
-        $this->logPath = sprintf('%s%s.log', $logDir, $this->channel);
-
-        $this->requestInterface = $requestInterface;
-    }
-
     abstract public function log($level, $message, $context = []);
 
     public function debug($message, $context = [])
--- src/WebServCo/Framework/FileLogger.php
+++ src/WebServCo/Framework/FileLogger.php
@@ -1,8 +1,30 @@
 <?php
 namespace WebServCo\Framework;
 
+use WebServCo\Framework\Exceptions\ApplicationException;
+use WebServCo\Framework\Interfaces\RequestInterface;
+
 final class FileLogger extends AbstractLogger implements \WebServCo\Framework\Interfaces\LoggerInterface
 {
+    protected $channel;
+    protected $logPath;
+    protected $requestInterface;
+
+    public function __construct($channel, $logDir, RequestInterface $requestInterface)
+    {
+        $this->channel = $channel;
+
+        if (!is_readable($logDir)) {
+            throw new ApplicationException('Log dir not readable');
+        }
+        if (!is_writable($logDir)) {
+            throw new ApplicationException('Log dir not writeable');
+        }
+        $this->logPath = sprintf('%s%s.log', $logDir, $this->channel);
+
+        $this->requestInterface = $requestInterface;
+    }
+
     public function log($level, $message, $context = [])
     {
         $data = sprintf(
