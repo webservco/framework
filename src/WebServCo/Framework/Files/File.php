f82578ee139d6e91c6ea0dd3cf5eb27431482ced
Radu Maier <ariademur@webserv.co>
2018-10-02 14:30:07 +0200

    FileException

--- src/WebServCo/Framework/Files/File.php
+++ src/WebServCo/Framework/Files/File.php
@@ -5,6 +5,9 @@ final class File extends AbstractFile
 {
     public function __construct($fileName, $fileData, $contentType = 'application/octet-stream')
     {
+        if (is_null($fileName)) {
+            throw new \WebServCo\Framework\Exceptions\FileException('File name is NULL');
+        }
         if (is_resource($fileData)) {
             $fileData = stream_get_contents($fileData);
         }
