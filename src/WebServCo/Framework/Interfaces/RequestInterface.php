8a238b2618f592f5bb6a129ce5fa247bf470aa11
Radu Maier <ariademur@webserv.co>
2018-10-24 16:02:42 +0200

    RequestInterface.getAcceptContentTypes

--- src/WebServCo/Framework/Interfaces/RequestInterface.php
+++ src/WebServCo/Framework/Interfaces/RequestInterface.php
@@ -3,6 +3,7 @@ namespace WebServCo\Framework\Interfaces;
 
 interface RequestInterface
 {
+    public function getAcceptContentTypes();
     public function getArgs();
     public function getMethod();
     public function getQuery();
