6aaa8be16099f0c5bf3a4263d38ca3205dbd4592
Radu Maier <ariademur@webserv.co>
2018-10-25 15:37:19 +0200

    NotImplementedException

---
+++ src/WebServCo/Framework/Exceptions/NotImplementedException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebServCo\Framework\Exceptions;
+
+final class NotImplementedException extends HttpException
+{
+    const CODE = 501;
+
+    public function __construct($message, \Exception $previous = null)
+    {
+        parent::__construct($message, self::CODE, $previous);
+    }
+}
