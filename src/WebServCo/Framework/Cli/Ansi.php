534ee0d67e9e17c9a2d00730c34765f4493098f6
Radu Maier <ariademur@webserv.co>
2018-10-31 12:47:23 +0100

    Cli/Ansi

---
+++ src/WebServCo/Framework/Cli/Ansi.php
@@ -0,0 +1,25 @@
+<?php
+namespace WebServCo\Framework\Cli;
+
+final class Ansi
+{
+    /**
+    * Select Graphic Rendition
+    *
+    * @see \WebServCo\Framework\Cli\Sgr
+    *
+    * @param string $string
+    * @param array $parameters ANSI SGR parameters (alterantively use the Sgr class constants)
+    *
+    * @return string
+    */
+    public static function sgr($string, array $parameters)
+    {
+        $result = '';
+        foreach ($parameters as $parameter) {
+            $result .= sprintf("\e[%sm", $parameter);
+        }
+        $result .= $string . sprintf("\e[%sm", Sgr::RESET);
+        return $result;
+    }
+}
