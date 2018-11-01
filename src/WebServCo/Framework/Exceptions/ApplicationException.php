3f6ee6f30dea5d0679420e9f0447f8b3bc321468
Radu Maier <ariademur@webserv.co>
2018-11-01 11:25:20 +0100

    ApplicationException can be extended

--- src/WebServCo/Framework/Exceptions/ApplicationException.php
+++ src/WebServCo/Framework/Exceptions/ApplicationException.php
@@ -1,13 +1,15 @@
 <?php
 namespace WebServCo\Framework\Exceptions;
 
-final class ApplicationException extends \Exception
+class ApplicationException extends \Exception
 {
-    public function __construct($message, $code = 500, \Exception $previous = null)
+    const CODE = 500;
+
+    public function __construct($message, $code = self::CODE, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
-    
+
     public function __toString()
     {
         return sprintf("%s: [%s]: %s\n", __CLASS__, $this->code, $this->message);
