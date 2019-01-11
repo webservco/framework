ba8ba2a8af9eaeebdc51e3d8f3f7545c2d34093d
Radu Maier <ariademur@webserv.co>
2019-01-11 12:09:21 +0100

    ArrayObject/Items

---
+++ src/WebServCo/Framework/ArrayObject/Items.php
@@ -0,0 +1,40 @@
+<?php
+namespace WebServCo\Framework\ArrayObject;
+
+class Items
+{
+    protected $arrayObject;
+
+    public function __construct(\ArrayObject $arrayObject)
+    {
+        $this->arrayObject = $arrayObject;
+    }
+
+    public function getArrayObject()
+    {
+        return $this->arrayObject;
+    }
+
+    public function set($index, $item)
+    {
+        $this->arrayObject->offsetSet($index, $item);
+        return true;
+    }
+
+    public function remove($index)
+    {
+        $this->arrayObject->offsetUnset();
+        $this->rebuildIndex();
+        return true;
+    }
+
+    protected function rebuildIndex()
+    {
+        $data = $this->arrayObject->getArrayCopy();
+        $this->arrayObject->exchangeArray([]);
+        foreach ($data as $item) {
+            $this->set(null, $item);
+        }
+        return true;
+    }
+}
