a8c3a3e5f61fdf7c0eba1f45bb79b7f6060d657c
Radu Maier <ariademur@webserv.co>
2019-02-01 09:43:44 +0100

    Update Interfaces/HttpBrowserInterface

--- src/WebServCo/Framework/Interfaces/HttpBrowserInterface.php
+++ src/WebServCo/Framework/Interfaces/HttpBrowserInterface.php
@@ -3,11 +3,13 @@ namespace WebServCo\Framework\Interfaces;
 
 interface HttpBrowserInterface
 {
-    public function get($url);
-
-    public function post($url, $data = []);
-
     public function setDebug($debug);
+
+    public function setMethod($method);
+
+    public function setRequestContentType($contentType);
+
+    public function setRequestData($data);
 
     public function setRequestHeader($name, $value);
 }
