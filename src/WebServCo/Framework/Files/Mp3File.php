6dec1a1d31140f81b9bb429aaad13e3f5a876287
Radu Maier <ariademur@webserv.co>
2019-04-08 16:17:09 +0200

    Files\Mp3File

---
+++ src/WebServCo/Framework/Files/Mp3File.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebServCo\Framework\Files;
+
+class Mp3File extends AbstractFile implements \WebServCo\Framework\Interfaces\FileInterface
+{
+    const CONTENT_TYPE = 'audio/mpeg';
+
+    public function __construct($fileName, $fileData)
+    {
+        parent::__construct($fileName, $fileData, self::CONTENT_TYPE);
+    }
+}
