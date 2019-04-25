65d99c9fcc037f6cde25f11eaec59984a0fdbe41
Radu Maier <ariademur@webserv.co>
2019-04-25 15:37:06 +0200

    Database/Abstractobject

---
+++ src/WebServCo/Framework/Database/AbstractObject.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebServCo\Framework\Database;
+
+abstract class AbstractObject
+{
+    protected $db;
+
+    public function __construct(\WebServCo\Framework\Interfaces\DatabaseInterface $db)
+    {
+        $this->db = $db;
+    }
+}
