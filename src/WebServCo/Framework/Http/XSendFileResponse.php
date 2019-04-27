daabc1425a2269fd32ccd6f82e38d0eda2d61358
Radu Maier <ariademur@webserv.co>
2019-04-27 17:57:33 +0200

    Http/XSendFileResponse

---
+++ src/WebServCo/Framework/Http/XSendFileResponse.php
@@ -0,0 +1,25 @@
+<?php
+namespace WebServCo\Framework\Http;
+
+class XSendFileResponse extends Response
+{
+    public function __construct($filePath, $outputFilename)
+    {
+        if (!is_readable($filePath)) {
+            throw new \WebServCo\Framework\Exceptions\NotFoundException('File not found');
+        }
+
+        parent::__construct(
+            null, // content
+            200, // statusCode
+            [
+                'Content-Type' => 'application/octet-stream',
+                'Content-Disposition' => sprintf(
+                    'attachment; filename=%s',
+                    $outputFilename
+                ),
+                'X-Sendfile' => $filePath,
+            ] // headers
+        );
+    }
+}
