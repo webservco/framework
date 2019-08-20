5f74b49c7f203905e81eb4dce133eec11f16c36f
Radu Maier <ariademur@webserv.co>
2019-08-20 10:19:26 +0200

    Arrays::get check if array

--- src/WebServCo/Framework/Utils/Arrays.php
+++ src/WebServCo/Framework/Utils/Arrays.php
@@ -5,6 +5,9 @@ final class Arrays
 {
     public static function get($array, $key, $defaultValue = false)
     {
+        if (!is_array($array)) {
+            return $defaultValue;
+        }
         return array_key_exists($key, $array) ? $array[$key] : $defaultValue;
     }
 
