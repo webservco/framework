7f9e6452a204b848621493996fcfe29697807112
Radu Maier <ariademur@webserv.co>
2019-10-24 10:51:55 +0200

    FileLoggerInterface

---
+++ src/WebServCo/Framework/Interfaces/FileLoggerInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace WebServCo\Framework\Interfaces;
+
+interface FileLoggerInterface extends LoggerInterface
+{
+    public function getLogDirectory();
+}
--- src/WebServCo/Framework/Log/FileLogger.php
+++ src/WebServCo/Framework/Log/FileLogger.php
@@ -4,25 +4,37 @@ namespace WebServCo\Framework\Log;
 use WebServCo\Framework\Exceptions\ApplicationException;
 use WebServCo\Framework\Interfaces\RequestInterface;
 
-final class FileLogger extends AbstractLogger implements \WebServCo\Framework\Interfaces\LoggerInterface
+final class FileLogger extends AbstractLogger implements \WebServCo\Framework\Interfaces\FileLoggerInterface
 {
     protected $channel;
+    protected $logDir;
     protected $logPath;
     protected $requestInterface;
 
     public function __construct($channel, $logDir, RequestInterface $requestInterface)
     {
         $this->channel = $channel;
+        $this->logDir = $logDir;
 
-        if (!is_readable($logDir)) {
+        if (!is_readable($this->logDir)) {
             throw new ApplicationException('Log directory not readable');
         }
-        if (!is_writable($logDir)) {
+        if (!is_writable($this->logDir)) {
             throw new ApplicationException('Log directory not writeable');
         }
-        $this->logPath = sprintf('%s%s.log', $logDir, $this->channel);
+        $this->logPath = sprintf('%s%s.log', $this->logDir, $this->channel);
 
         $this->requestInterface = $requestInterface;
+    }
+
+    public function clear()
+    {
+        return file_put_contents($this->logPath, null);
+    }
+
+    public function getLogDirectory()
+    {
+        return $this->logDir;
     }
 
     public function log($level, $message, $context = [])
@@ -39,10 +51,5 @@ final class FileLogger extends AbstractLogger implements \WebServCo\Framework\In
             $data .= sprintf('%s%s', var_export($context, true), PHP_EOL);
         }
         return file_put_contents($this->logPath, $data, FILE_APPEND);
-    }
-
-    public function clear()
-    {
-        return file_put_contents($this->logPath, null);
     }
 }
