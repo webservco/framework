d57ee36c93e30f60017a2f967b590eda455bfa52
Radu Maier <ariademur@webserv.co>
2020-01-22 10:45:36 +0100

    method organization

--- src/WebServCo/Framework/ErrorHandler.php
+++ src/WebServCo/Framework/ErrorHandler.php
@@ -3,59 +3,6 @@ namespace WebServCo\Framework;
 
 final class ErrorHandler
 {
-    /**
-     * Registers as the error handler.
-     *
-     * @return bool
-     */
-    public static function set()
-    {
-        self::disableErrorDisplay();
-        set_error_handler(['\WebServCo\Framework\ErrorHandler', 'throwErrorException']);
-        return true;
-    }
-
-    /**
-     * Disable error display.
-     */
-    protected static function disableErrorDisplay()
-    {
-        ini_set('display_errors', 0);
-    }
-
-    /**
-     * Restores default error handler.
-     *
-     * @return bool
-     */
-    public static function restore()
-    {
-        return restore_error_handler();
-    }
-
-    /**
-     * Throws ErrorException.
-     *
-     * @param int $errno Error level
-     * @param string $errstr Error message
-     * @param string $errfile Filename the error was raised in
-     * @param int $errline Line number the error was raised at
-     *
-     * @throws \ErrorException
-     */
-    public static function throwErrorException($errno, $errstr, $errfile, $errline)
-    {
-        if (error_reporting() & $errno) { //check if error level is set
-            throw new \ErrorException(
-                sprintf('%s: %s', self::getErrorTypeString($errno), $errstr),
-                0,
-                $errno,
-                $errfile,
-                $errline
-            );
-        }
-    }
-
     public static function getErrorTypeString($type)
     {
         switch ($type) {
@@ -94,5 +41,58 @@ final class ErrorHandler
             default:
                 return 'Unknown';
         }
+    }
+
+    /**
+     * Restores default error handler.
+     *
+     * @return bool
+     */
+    public static function restore()
+    {
+        return restore_error_handler();
+    }
+
+    /**
+     * Registers as the error handler.
+     *
+     * @return bool
+     */
+    public static function set()
+    {
+        self::disableErrorDisplay();
+        set_error_handler(['\WebServCo\Framework\ErrorHandler', 'throwErrorException']);
+        return true;
+    }
+
+    /**
+     * Throws ErrorException.
+     *
+     * @param int $errno Error level
+     * @param string $errstr Error message
+     * @param string $errfile Filename the error was raised in
+     * @param int $errline Line number the error was raised at
+     *
+     * @throws \ErrorException
+     */
+    public static function throwErrorException($errno, $errstr, $errfile, $errline)
+    {
+        if (error_reporting() & $errno) { //check if error level is set
+            throw new \ErrorException(
+                sprintf('%s: %s', self::getErrorTypeString($errno), $errstr),
+                0,
+                $errno,
+                $errfile,
+                $errline
+            );
+        }
+    }
+
+    /**
+     * Disable error display.
+     */
+    protected static function disableErrorDisplay()
+    {
+        ini_set('display_errors', 0);
     }
 }
