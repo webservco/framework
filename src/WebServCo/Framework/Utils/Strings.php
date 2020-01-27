d43eb8f23faafefb28acb90724e9ab6b753c18f0
Radu Maier <ariademur@webserv.co>
2020-01-27 14:09:58 +0100

    cleanup

--- src/WebServCo/Framework/Utils/Strings.php
+++ src/WebServCo/Framework/Utils/Strings.php
@@ -25,7 +25,7 @@ final class Strings
     public static function getContextAsString($context)
     {
         ob_start();
-        /** @scrutinizer ignore-call */var_dump($context);
+        var_dump($context);
         return ob_get_clean();
     }
 
