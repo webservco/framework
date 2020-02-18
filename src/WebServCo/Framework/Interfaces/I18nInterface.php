5ae4c14df7f053019062445e4334e330d229bc8d
Radu Maier <ariademur@webserv.co>
2020-02-18 18:48:59 +0100

    I18nInterface

---
+++ src/WebServCo/Framework/Interfaces/I18nInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace WebServCo\Framework\Interfaces;
+
+interface I18nInterface
+{
+    public function getLanguage();
+    public function init($projectPath, $lang = null);
+    public function setLanguage($lang, $translationsPath = null);
+}
