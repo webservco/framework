3e0df0047276af2b6169b6890ce3f9e4ba6f98c7
Radu Maier <ariademur@webserv.co>
2020-05-05 18:36:34 +0200

    Arrays.AbstractValidatedClass

---
+++ src/WebServCo/Framework/Arrays/AbstractValidatedClass.php
@@ -0,0 +1,30 @@
+<?php
+namespace WebServCo\Framework\Arrays;
+
+abstract class AbstractValidatedClass
+{
+    abstract protected function getRequiredProperties();
+
+    public function __construct($data = [])
+    {
+        foreach (get_object_vars($this) as $property => $default) {
+            if (array_key_exists($property, $data)) {
+                $this->{$property} = $data[$property];
+            }
+        }
+        $this->validate();
+    }
+
+    protected function validate()
+    {
+        $required = $this->getRequiredProperties();
+        foreach ($required as $item) {
+            if (empty($this->{$item})) {
+                throw new \WebServCo\Framework\Exceptions\Validation\RequiredArgumentException(
+                    sprintf('Empty required parameter: %s', $item)
+                );
+            }
+        }
+        return true;
+    }
+}
