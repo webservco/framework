cd4678ef8ff1260c51232e70e9fb619c1b5913dd
Radu Maier <ariademur@webserv.co>
2020-06-04 09:06:06 +0200

    RunnerException, InterruptedException

---
+++ src/WebServCo/Framework/Exceptions/InterruptedException.php
@@ -0,0 +1,6 @@
+<?php
+namespace WebServCo\Framework\Exceptions;
+
+final class InterruptedException extends RunnerException
+{
+}
---
+++ src/WebServCo/Framework/Exceptions/RunnerException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebServCo\Framework\Exceptions;
+
+class RunnerException extends ApplicationException
+{
+    const CODE = 0;
+
+    public function __construct($message, \Exception $previous = null)
+    {
+        parent::__construct($message, self::CODE, $previous);
+    }
+}
