fd70ec1bae97b2d4ff676d76d2b148c29709f0c4
Radu Maier <ariademur@webserv.co>
2020-07-24 12:04:14 +0200

    LoggerInterface warning()

--- src/WebServCo/Framework/Interfaces/LoggerInterface.php
+++ src/WebServCo/Framework/Interfaces/LoggerInterface.php
@@ -8,4 +8,5 @@ interface LoggerInterface
     public function error($message, $context = []);
     public function info($message, $context = []);
     public function log($level, $message, $context = []);
+    public function warning($message, $context = []);
 }
