fbc41eb5e665afb39c01ec293590e06834c8fb57
Radu Maier <ariademur@webserv.co>
2020-11-27 16:52:25 +0100

    Upgrade redirect status codes.

--- src/WebServCo/Framework/Traits/ResponseUrlTrait.php
+++ src/WebServCo/Framework/Traits/ResponseUrlTrait.php
@@ -26,18 +26,25 @@ trait ResponseUrlTrait
     final protected function getReloadResponse($removeParameters = [])
     {
         $url = $this->request()->getUrl($removeParameters);
-        return $this->getRedirectUrlResponse($url);
+        // "The HTTP 205 Reset Content response status tells the client to reset the document view,
+        // so for example to clear the content of a form, reset a canvas state, or to refresh the UI"
+        return $this->getRedirectUrlResponse($url, 205);
     }
 
     /**
      * Redirect to a full URL.
      * This method returns a Response object that needs to be in turn returned to the application.
+     *
+     * "303 See Other (since HTTP/1.1)
+     * The response to the request can be found under another URI using the GET method.
+     * When received in response to a POST (or PUT/DELETE), the client should presume that the server has received
+     * the data and should issue a new GET request to the given URI."
      */
-    final protected function getRedirectUrlResponse($url)
+    final protected function getRedirectUrlResponse($url, $statusCode = 303)
     {
         return new \WebServCo\Framework\Http\Response(
             null,
-            302,
+            $statusCode,
             ['Location' => $url]
         );
     }
