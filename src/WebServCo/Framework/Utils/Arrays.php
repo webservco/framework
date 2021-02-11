bf08a44f2d23c4429019c0cab1116c321407ff25
Radu Maier <ariademur@webserv.co>
2021-02-11 11:07:09 +0100

    Fix typehint

--- src/WebServCo/Framework/Utils/Arrays.php
+++ src/WebServCo/Framework/Utils/Arrays.php
@@ -106,7 +106,7 @@ final class Arrays
     }
 
     /**
-    * @param array<mixed> $array
+    * @param array<int|float|string,int|float|string> $array
     */
     public static function toUrlQueryString(array $array): ?string
     {
