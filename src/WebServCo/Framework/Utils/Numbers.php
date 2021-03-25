0552c3dcc3417d902f61ab32b0b76432dedc9a0b
Radu Maier <ariademur@webserv.co>
2021-03-25 10:31:00 +0100

    Numbers.isEmpty

---
+++ src/WebServCo/Framework/Utils/Numbers.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\Framework\Utils;
+
+final class Numbers
+{
+    /**
+    * @param mixed $number
+    */
+    public static function isEmpty($number): bool
+    {
+        $number = \floatval($number);
+
+        return 0 !== $number;
+    }
+}
