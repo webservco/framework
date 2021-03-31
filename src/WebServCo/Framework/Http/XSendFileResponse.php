aff652365a62fdc0b042bee8c1a802b3c488cb17
Radu Maier <ariademur@webserv.co>
2021-03-31 15:28:42 +0200

    XSendFileResponse custom content type

--- src/WebServCo/Framework/Http/XSendFileResponse.php
+++ src/WebServCo/Framework/Http/XSendFileResponse.php
@@ -3,7 +3,7 @@ namespace WebServCo\Framework\Http;
 
 class XSendFileResponse extends Response
 {
-    public function __construct($filePath, $outputFilename)
+    public function __construct($filePath, $outputFilename, $contentType = 'application/octet-stream')
     {
         if (!is_readable($filePath)) {
             throw new \WebServCo\Framework\Exceptions\NotFoundException('File not found.');
@@ -13,7 +13,7 @@ class XSendFileResponse extends Response
             null, // content
             200, // statusCode
             [
-                'Content-Type' => 'application/octet-stream',
+                'Content-Type' => $contentType,
                 'Content-Disposition' => sprintf(
                     'attachment; filename="%s"',
                     $outputFilename
