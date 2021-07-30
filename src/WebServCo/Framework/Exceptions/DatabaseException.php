880f24ee23b73318fe5978435da9aac18a744988
Radu Maier <ariademur@webserv.co>
2021-07-30 17:08:14 +0200

    Improve PDOException handling.

--- src/WebServCo/Framework/Exceptions/DatabaseException.php
+++ src/WebServCo/Framework/Exceptions/DatabaseException.php
@@ -15,16 +15,23 @@ final class DatabaseException extends ApplicationException
     {
         $code = self::CODE;
 
-        if ($previous instanceof \PDOException) {
-            if (!empty($previous->errorInfo[1])) {
-                $code = $previous->errorInfo[1];
-                $this->sqlState = $previous->errorInfo[0];
-            }
+        switch (true) {
+            case $previous instanceof \PDOException:
+                if (!empty($previous->errorInfo[1])) {
+                    $code = $previous->errorInfo[1];
+                    $this->sqlState = $previous->errorInfo[0];
+                }
 
-            if (!empty($previous->errorInfo[2])) {
-                // cleaner error message without all the codes.
-                $message = $previous->errorInfo[2];
-            }
+                if (!empty($previous->errorInfo[2])) {
+                    // cleaner error message without all the codes.
+                    $message = $previous->errorInfo[2];
+                }
+                break;
+            case $previous instanceof DatabaseException:
+                // A \PDOException that was re-thrown
+                $code = $previous->getCode();
+                $this->sqlState = $previous->getSqlState();
+                break;
         }
 
         parent::__construct($message, $code, $previous);
