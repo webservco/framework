7f23f25adbe1b3062436511f1911408566365d42
Radu Maier <ariademur@webserv.co>
2021-11-16 11:22:05 +0100

    AbstractUser.ERR_NOT_FOUND

--- src/WebServCo/Framework/AbstractUser.php
+++ src/WebServCo/Framework/AbstractUser.php
@@ -7,6 +7,7 @@ namespace WebServCo\Framework;
 abstract class AbstractUser extends \WebServCo\Framework\AbstractLibrary
 {
 
-    public const ERR_LOGIN = 'ERR_LOGIN';
     public const ERR_DISABLED = 'ERR_DISABLED';
+    public const ERR_LOGIN = 'ERR_LOGIN';
+    public const ERR_NOT_FOUND = 'ERR_NOT_FOUND';
 }
