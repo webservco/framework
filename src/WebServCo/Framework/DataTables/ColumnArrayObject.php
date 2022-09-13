83ccc62dc79870a81e602210e1bf2f23b6b991dc
Radu Maier <ariademur@webserv.co>
2022-09-13 15:50:00 +0200

    phpstan config and related fixes. Remove tests from composer run.

--- src/WebServCo/Framework/DataTables/ColumnArrayObject.php
+++ src/WebServCo/Framework/DataTables/ColumnArrayObject.php
@@ -2,9 +2,16 @@
 
 namespace WebServCo\Framework\DataTables;
 
+/**
+* @extends \ArrayObject<int, \WebServCo\Framework\DataTables\Column>
+*/
 class ColumnArrayObject extends \ArrayObject
 {
-    public function offsetSet($name, $value)
+    /**
+    * @param mixed $name
+    * @param mixed $value
+    */
+    public function offsetSet($name, $value): void
     {
         if (!($value instanceof Column)) {
             throw new \InvalidArgumentException('Only Column objects allowed.');
--- src/WebServCo/Framework/DataTables/OrderArrayObject.php
+++ src/WebServCo/Framework/DataTables/OrderArrayObject.php
@@ -2,9 +2,16 @@
 
 namespace WebServCo\Framework\DataTables;
 
+/**
+* @extends \ArrayObject<int, \WebServCo\Framework\DataTables\Order>
+*/
 class OrderArrayObject extends \ArrayObject
 {
-    public function offsetSet($name, $value)
+    /**
+    * @param mixed $name
+    * @param mixed $value
+    */
+    public function offsetSet($name, $value): void
     {
         if (!($value instanceof Order)) {
             throw new \InvalidArgumentException('Only Order objects allowed.');
